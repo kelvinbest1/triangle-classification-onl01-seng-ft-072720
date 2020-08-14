9616636d3684492a4e6e0116d07a75c310a0a9ff
kelvin lanier <kelvinlanier@yahoo.com>
2020-08-14 16:48:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,6 +5,11 @@ class Triangle
     @side_2 = side_2
     @side_3 = side_3
   end
+  
+   class TriangleError < StandardError
+    # triangle error code
+  end
+end
 
   def kind()
     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
